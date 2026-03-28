83177af46fe1456db0d66d51ae7fd9059d3e857b
aamirr-ali <amirwide@gmail.com>
2026-03-28 21:51:10 +0530

    fix

--- src/com/android/settings/ui/personalizationSettingsController.kt
+++ src/com/android/settings/ui/personalizationSettingsController.kt
@@ -41,7 +41,7 @@ class personalizationSettingsController(context: Context) : AbstractPreferenceCo
             R.id.spoof to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsSpoofActivity")),
             R.id.bottom_options to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsBottomActivity")),
             R.id.statusbar to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsStatusbarActivity")),
-            R.id.botton_options to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsButtonActivity")),
+            R.id.button_options to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsButtonActivity")),
             R.id.about to Intent().setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$PersonalizationsAboutActivity"))
        )
 
